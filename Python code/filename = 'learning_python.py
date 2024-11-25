81f122cd182cb3cc271b7a34130473aa8c7a2b8a
nmancha865 <nmancha@mymail.tstc.edu>
2024-11-25 10:10:43 -0600

    Update filename = 'learning_python.py

--- Python code/filename = 'learning_python.py
+++ Python code/filename = 'learning_python.py
@@ -1,8 +1,9 @@
+#tells the code which file it is supposed to open
 file = "Documents/learningpython.txt"
-
+#tells it just to read the file 
 for i in range(3):
     print(open(file).read())
-
+#to print all the lines in the file 
 with open(file, 'r') as fin:
     for lines in fin:
         print(lines)
@@ -15,3 +16,7 @@ cleaned_text = []
 for text in my_text:
     cleaned_text.append(text.rstrip("\n"))
 print(cleaned_text)
+
+
+
+This Python code has you create a file in your file explorer and write something in it and then open the file in python and print whats in the file. 
